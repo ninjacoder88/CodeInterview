783387609e357345ae773d813d8eee334b792f5b
Tommy Harris <tomharris@idtdna.com>
2019-12-09 08:07:25 -0600

    added proxy for console

---
+++ src/Utility/ConsoleWriter.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CodeInterview.Utility
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ src/Utility/IWriter.cs
@@ -0,0 +1,9 @@
+namespace CodeInterview.Utility
+{
+    public interface IWriter
+    {
+        public void Write(string message);
+
+        public void WriteLine(string message);
+    }
+}
